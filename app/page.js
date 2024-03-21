4cab942cce9ec3791b0cc51265a7156f2e1687f5
mod728 <phallus-phaser@hotmail.com>
2024-03-21 17:54:10 +0900

    code for Chapter 7

---
+++ app/page.js
@@ -0,0 +1,10 @@
+const ReadAllItems = () => {
+    return (
+        <div>
+            <h1 className="h1-style">こんにちは</h1>
+            <h3>さようなら</h3>
+        </div>
+    )
+} 
+
+export default ReadAllItems
